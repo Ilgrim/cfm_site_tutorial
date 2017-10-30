312a560bd27bd1d87ffa3ef6a7dbcc7e80ce0ab3
Rudy Rocha <rudyaldair@hotmail.com>
2017-10-29 23:32:58 -0600

    Unico commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Import dependencies
+import React from 'react'
+import { render } from 'react-dom'
+//Import component
+import Site from "./components/Site"
+//Render method in your div#root
+render(
+	<Site />,
+	document.getElementById('root')
+)
